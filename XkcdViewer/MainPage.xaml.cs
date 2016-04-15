1e7e891d2c9086713a420699e9d8503a374b5035
Lance McCarthy <lance.mccarthy@telerik.com>
2016-04-15 18:20:10 -0400

    Initial commit

---
+++ XkcdViewer/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XkcdViewer
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+

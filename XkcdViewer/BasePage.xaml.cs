7e01dbe1aea7475b38e12f665fec6a33ee2ad8de
Lance McCarthy <lance.mccarthy@telerik.com>
2016-09-27 17:02:25 -0400

    Updated Telerik UI for Xamarin to use nuget instead of direct references

--- XkcdViewer/BasePage.xaml.cs
+++ XkcdViewer/BasePage.xaml.cs
@@ -9,18 +9,12 @@ namespace XkcdViewer
             InitializeComponent();
         }
 
-        /// <summary>
-        /// Instantiate the root NavigationPage with the first ContentPage
-        /// </summary>
-        /// <param name="page">ContentPage to be shown at launch</param>
         public BasePage(Page page)
         {
             InitializeComponent();
-
-            //push the passed Page into the nav stack
+            
             this.Navigation.PushAsync(page);
-
-            //Sets the TitleBar icon
+            
             NavigationPage.SetTitleIcon(page, "XKCD_small_logo.png");
         }
     }

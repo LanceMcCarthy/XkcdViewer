3f2e41d9a4f016132e6d21d8ecf146c851164a06
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-04-23 14:19:12 -0400

    Initial MAUI migration
    Migrated the entire project code to .NET MAUI, except publishing and code signing settings.

---
+++ XkcdViewer.Maui/ViewModels/DetailsPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using CommonHelpers.Common;
+using XkcdViewer.Maui.Models;
+
+namespace XkcdViewer.Maui.ViewModels;
+
+public class DetailsPageViewModel : ViewModelBase
+{
+    private Comic selectedComic;
+
+    public DetailsPageViewModel() { }
+
+    public Comic SelectedComic
+    {
+        get => selectedComic;
+        set
+        {
+            if (SetProperty(ref selectedComic, value))
+            {
+                this.Title = $"#{SelectedComic.Num}";
+            }
+        }
+    }
+}
---
+++ XkcdViewer.Maui/Views/DetailsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using XkcdViewer.Maui.Models;
+
+namespace XkcdViewer.Maui.Views;
+
+public partial class DetailsPage : ContentPage
+{
+    public DetailsPage(Comic comic)
+    {
+        InitializeComponent();
+        ViewModel.SelectedComic = comic;
+    }
+}

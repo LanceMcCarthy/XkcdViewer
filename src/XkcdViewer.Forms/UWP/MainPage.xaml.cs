62070d44761c8041e4cbe38971a3873994b2f27c
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-04-23 14:43:30 -0400

    Move code to src folder

---
+++ src/XkcdViewer.Forms/UWP/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+using FFImageLoading.Forms.Platform;
+
+namespace XkcdViewer.Forms.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
+            LoadApplication(new Forms.App());
+        }
+    }
+}

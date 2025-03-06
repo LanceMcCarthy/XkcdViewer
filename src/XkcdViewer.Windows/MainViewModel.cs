e372c6fa09a69cc28752bb28a13574c98e6f540a
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-03-05 22:57:48 -0500

    WIP 2 - Extraction to class library complete. Got Windows working on a baseline, MAUI working again

---
+++ src/XkcdViewer.Windows/MainViewModel.cs
@@ -0,0 +1,115 @@
+﻿using CommonHelpers.Common;
+using CommonHelpers.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CommonHelpers.Services;
+using XkcdViewer.Common.Models;
+using XkcdViewer.Common.Services;
+using System.Collections.Generic;
+using Microsoft.UI.Xaml;
+using Telerik.UI.Xaml.Controls.Input;
+
+namespace XkcdViewer.Windows;
+
+public class MainViewModel : ViewModelBase
+{
+    private readonly ComicDataService comicDataService;
+    private Comic? currentComic;
+    private Visibility getNewComicButtonIsVisible;
+    private string selectedSegment = "Comics";
+
+    public MainViewModel()
+    {
+        comicDataService = new ComicDataService(new XkcdApiService());
+
+        FetchComicCommand = new RelayCommand(async () => await FetchComicAsync());
+        ShareCommand = new RelayCommand(async () => await ShareItemAsync());
+        ToggleFavoriteCommand = new RelayCommand(async ()=> await ToggleFavorite(CurrentComic));
+    }
+
+    public ObservableCollection<Comic> Comics { get; } = [];
+
+    public Comic? CurrentComic
+    {
+        get => currentComic;
+        set
+        {
+            if (SetProperty(ref currentComic, value))
+            {
+                GetNewComicButtonIsVisible = Comics.LastOrDefault() == currentComic ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+    }
+
+    public Visibility GetNewComicButtonIsVisible
+    {
+        get => getNewComicButtonIsVisible;
+        set => SetProperty(ref getNewComicButtonIsVisible, value);
+    }
+
+    public string SelectedSegment
+    {
+        get => selectedSegment;
+        set => SetProperty(ref selectedSegment, value);
+    }
+
+    public RelayCommand FetchComicCommand { get; set; }
+
+    public RelayCommand ShareCommand { get; set; }
+
+    public RelayCommand ToggleFavoriteCommand { get; set; }
+
+    public async Task FetchComicAsync()
+    {
+        try
+        {
+            if (IsBusy)
+                return;
+
+            IsBusy = true;
+
+            await comicDataService.GetComic(Comics, Comics.LastOrDefault()?.Num - 1);
+
+            CurrentComic = Comics.Last();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"FetchComicAsync Exception\r\n{ex}");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private async Task ToggleFavorite(Comic? comic)
+    {
+        if (comic != null)
+        {
+            comic.IsFavorite = !comic.IsFavorite;
+
+            await comicDataService.SaveComicsAsync(Comics);
+        }
+    }
+
+    public async Task ShareItemAsync()
+    {
+        if (string.IsNullOrEmpty(currentComic?.Img))
+            return;
+
+        //await Share.Default.RequestAsync(new ShareTextRequest
+        //{
+        //    Title = Title ?? "xkcd",
+        //    Text = currentComic.Transcript ?? "",
+        //    Uri = currentComic.Img
+        //});
+    }
+
+    public async Task InitialLoadAsync()
+    {
+        await comicDataService.LoadComicsAsync(Comics);
+    }
+}
--- src/XkcdViewer.Windows/MainWindow.xaml.cs
+++ src/XkcdViewer.Windows/MainWindow.xaml.cs
@@ -1,36 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using System.Collections.Generic;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace XkcdViewer.Windows;
 
-namespace XkcdViewer.Windows
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    private MainViewModel vm;
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-        }
+        this.InitializeComponent();
+        RootGrid.DataContext = vm =new MainViewModel();
+        RootGrid.Loaded += (s,e)=>((MainViewModel)((Grid)s).DataContext)?.InitialLoadAsync();
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+
+        SegmentedControl.ItemsSource = new List<string> { "Comics", "PDFs" };
+        SegmentedControl.SelectedIndex = 0;
+    }
+
+    private void Segements_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (SegmentedControl.SelectedItem == "Comics")
         {
-            myButton.Content = "Clicked";
+            Presenter.ContentTemplate = RootGrid.Resources["ComicTemplate"] as DataTemplate;
+        }
+        else
+        {
+            Presenter.ContentTemplate = RootGrid.Resources["PdfTemplate"] as DataTemplate;
         }
     }
 }

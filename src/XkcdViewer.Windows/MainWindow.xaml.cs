54a8cc3cf5ba89b09cb611b2cf8a8bb942419d91
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-03-08 12:57:49 -0500

    WinUI - custom paginatin item style

--- src/XkcdViewer.Windows/MainWindow.xaml.cs
+++ src/XkcdViewer.Windows/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
-using Microsoft.Graphics.Imaging;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.Windows.AI.ContentModeration;
-using Microsoft.Windows.AI.Generative;
-using Microsoft.Windows.Management.Deployment;
-using System;
-using System.Diagnostics;
-using System.IO;
-using System.Net.Http;
-using Windows.Graphics.Imaging;
-using Windows.Media.Core;
-using Windows.Media.SpeechSynthesis;
-using Windows.Storage;
-using Windows.Storage.Streams;
 
 namespace XkcdViewer.Windows;
 
